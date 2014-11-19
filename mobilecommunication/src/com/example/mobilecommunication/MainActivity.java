1eb184a14ebadb384e353415aa5cb4a4d8e89966
senoramarillo <andy.lachkrampf@googlemail.com>
2014-11-19 04:03:24 +0100

    - camera function demo + button + imageView

--- mobilecommunication/src/com/example/mobilecommunication/MainActivity.java
+++ mobilecommunication/src/com/example/mobilecommunication/MainActivity.java
@@ -1,41 +1,41 @@
 package com.example.mobilecommunication;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity {
-	
-	private final String TAG = "DemoButtonApp";
+	ImageView iv;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        setupMessageButton();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-	private void setupMessageButton() {
-		//1. Get a reference to the button.
-		Button messageButton = (Button)findViewById(R.id.btnDisplayMessage);
-		
-		//2. Set the click listener to my code.
-		View.OnClickListener myListener = new View.OnClickListener() {
+		iv = (ImageView) findViewById(R.id.imageView);
+		Button btn = (Button) findViewById(R.id.takePhoto);
+		btn.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				Log.i(TAG, "You clicked the button!");
-				Toast.makeText(MainActivity.this, 
-						      "You clicked it!", 
-						      Toast.LENGTH_LONG)
-						      .show();
-				
+				Intent intent = new Intent(
+						android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(intent, 0);
 			}
-		};
-		messageButton.setOnClickListener(myListener);
+		});
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == 0) {
+			Bitmap theImage = (Bitmap) data.getExtras().get("data");
+			iv.setImageBitmap(theImage);
+		}
 	}
 }
