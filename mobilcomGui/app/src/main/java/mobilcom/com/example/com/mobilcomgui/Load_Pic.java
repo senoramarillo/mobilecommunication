b510aafd2d16fb4e4df7af3346292ef3665c7bbb
senoramarillo <andy.lachkrampf@googlemail.com>
2015-01-04 03:14:43 +0100

    kleinen Änderungen der Methode loadImageClick(View v)

--- mobilcomGui/app/src/main/java/mobilcom/com/example/com/mobilcomgui/Load_Pic.java
+++ mobilcomGui/app/src/main/java/mobilcom/com/example/com/mobilcomgui/Load_Pic.java
@@ -3,32 +3,63 @@ package mobilcom.com.example.com.mobilcomgui;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
-/**
- * Created by Malte on 28.12.2014.
- */
 public class Load_Pic extends Activity {
 
-    Button btn_edit;
-    Bitmap img;
+    private static final int REQUEST_CODE = 1;
+    //Button btn_edit;
+    private Bitmap bitmap;
+    private ImageView imageView;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_pic);
-        Intent intent = getIntent();
+        imageView = (ImageView) findViewById(R.id.result);
+        //Intent intent = getIntent();
     }
 
     public void editClick(View v) {
         Intent i = new Intent(this, Edit.class);
-        i.putExtra("img", img);
+        i.putExtra("img", bitmap);
         startActivity(i);
     }
-    public void loadImageClick(View v){
-        //TODO start promt to load data browser
+
+    //Abrufen der Bilder erfolgt über loadImageClick
+    public void loadImageClick(View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, REQUEST_CODE);
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        InputStream stream = null;
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK)
+            try {
+                // recyle unused bitmaps
+                if (bitmap != null) {
+                    bitmap.recycle();
+                }
+                stream = getContentResolver().openInputStream(data.getData());
+                bitmap = BitmapFactory.decodeStream(stream);
+
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        if (stream != null)
+            try {
+                stream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
 }
